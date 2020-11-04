b144176edb9558b86fb5c71f25dd7cf51ce7beb2
“Simran <itsimmi5@gmail.com>
2020-11-03 22:08:29 -0500

    Form validation is working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import { useState, useEffect} from 'react';
+import { useState } from 'react';
 
 function App() {
   const [userInputName, setUserInputName] = useState('');
   const [userInputNumber, setUserInputNumber] = useState('');
   const [userInputEmail, setUserInputEmail] = useState('');
+  const [isFormSubmit, setIsFormSubmit] = useState(false);
   const [isUserNameValid, setIsUserNameValid] = useState(false);
-
+  const [isUserNumValid, setIsUserNumValid] = useState(false);
+  const [isUserEmailValid, setIsUserEmailValid] = useState(false);
   const validEmailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
   let onNameChange = (event) => {
     setUserInputName(event.target.value);
   }
@@ -21,12 +24,33 @@ function App() {
   }
 
   let onSubmit = () => {
-    if (userInputName.length >= 4 && validEmailFormat.test(userInputEmail) && userInputNumber.length >= 10) {
-      console.log('values are valid');
+    validateName();
+    validateNum();
+    validateEmail();
+    setIsFormSubmit(true);
+  }
+
+  let validateName = () => {
+    if(userInputName.length > 4) {
       setIsUserNameValid(true);
-    } else { 
-      console.log('values are invalid');
+    } else {
       setIsUserNameValid(false);
+    }
+  }
+
+  let validateNum = () => {
+    if(validEmailFormat.test(userInputEmail)) {
+      setIsUserEmailValid(true);
+    } else {
+      setIsUserEmailValid(false);
+    }
+  }
+
+  let validateEmail = () => {
+    if(userInputNumber.length === 10) {
+      setIsUserNumValid(true);
+    } else {
+      setIsUserNumValid(false);
     }
   }
 
@@ -34,18 +58,34 @@ function App() {
     <div className='App'>
       <label>
         Name
-        <input type="text" value={userInputName} onChange={onNameChange} /> 
-        {/* {isUserNameValid ? null : <p>Enter valid name</p>} */}
+        <input
+          type="text"
+          value={userInputName}
+          onChange={onNameChange}
+        /> 
+        {isUserNameValid ? null : isFormSubmit ? <p>Enter valid name</p> : null}
       </label>
-      <label>
+      <label> 
         Phone number
-        <input type="number" value={userInputNumber} onChange={onNumberChange}/> 
+        <input
+          type="number"
+          value={userInputNumber}
+          onChange={onNumberChange}
+        /> 
+        {isUserNumValid ? null : isFormSubmit ? <p>Enter valid phone number</p> : null}
       </label>
       <label>
         Email
-        <input type="email" value={userInputEmail} onChange={onEmailChange}/> 
+        <input
+          type="email"
+          value={userInputEmail}
+          onChange={onEmailChange}
+        /> 
+        {isUserEmailValid ? null : isFormSubmit ? <p>Enter valid email</p> : null}
       </label>
       <button type='submit' onClick={onSubmit}> Submit </button>
+      
+      {isUserNameValid && isUserNumValid && isUserEmailValid && setIsFormSubmit ? <h1> This form is submited</h1> : null}
     </div>
   );
 }
